652ee964c24ad0af5f5fd29d104e3894417176c8
JarredTD <jarred.desrosiers@yahoo.com>
2024-01-23 17:48:34 -0500

    feat!: Remove depreciated methods/enums regarding Instruction based movement

--- Arduino/Patchy/PatchyUtil.h
+++ Arduino/Patchy/PatchyUtil.h
@@ -12,28 +12,6 @@ public:
     };
 
     /**
-     * @brief Calculate a hash value for the input string.
-     *
-     * @param str The input string for which the hash is calculated.
-     * @return The calculated hash value as a long integer.
-     */
-    static long hashString(const char *str);
-
-    /**
-     * Enum defining different directions as hashes.
-     */
-    enum class Instruction : long
-    {
-        Up = 5863882,
-        Down = 2090192221,
-        Left = 2090468272,
-        Right = 273236323,
-        Grab = 2090302561,
-        Rst = 1935050542,
-        Manual = 221964931
-    };
-
-    /**
      * Enum defining different completion statuses .
      */
     enum class Status
@@ -42,15 +20,6 @@ public:
         Failure,
         Success,
         Invalid
-    };
-
-    /**
-     * Enum defining different axes.
-     */
-    enum class Axis
-    {
-        X,
-        Y
     };
 
     enum class State 
